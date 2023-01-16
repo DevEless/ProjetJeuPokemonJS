b061ecbaec6f51ddd40284de0b2cba33b8dcf514
Quent_DLM <qdelameilleure@gmail.com>
2023-01-16 18:40:44 +0100

    création du fichier fonction

---
+++ projet_app_meteo/public/js/modules/fonctions.js
@@ -0,0 +1,40 @@
+import {
+image, pokeNumber, pokeName, pokeType , image2, pokeNumber2, pokeName2, pokeType2
+} from "./modules/variable.js"
+import { image2 } from "./variable.js";
+
+
+const changePokemon = async () => {
+    let randomnumber = Math.ceil(Math.random() *900 + 1);
+
+    let requestString = `https://pokeapi.co/api/v2/pokemon/${randomnumber}
+    `
+    let data = await fetch(requestString)
+    console.log(data)
+    
+    let response = await data.json()
+    console.log(response)
+
+    image.src = response.sprites.front_default;
+    pokeNumber.textContent = `#${response.id}`;
+    pokeName.textContent = response.name;
+    pokeType.textContent = response.types[0].type.name;
+};
+
+
+const changePokemon2 = async () => {
+    let randomnumber = Math.ceil(Math.random() *900 + 1);
+
+    let requestString = `https://pokeapi.co/api/v2/pokemon/${randomnumber}
+    `
+    let data = await fetch(requestString)
+    console.log(data)
+    
+    let response = await data.json()
+    console.log(response)
+
+    image2.src = response.sprites.front_default;
+    pokeNumber2.textContent = `#${response.id}`;
+    pokeName2.textContent = response.name;
+    pokeType2.textContent = response.types[0].type.name;
+};
--- projet_app_meteo/public/js/modules/variable.js
+++ projet_app_meteo/public/js/modules/variable.js
@@ -1,5 +1,10 @@
-export let button = document.getElementById("button")
+
 export let image = document.getElementById("image")
 export let pokeNumber = document.getElementById("number")
 export let pokeName = document.getElementById("name")
 export let pokeType = document.getElementById("type")
+
+export let image2 = document.getElementById("image2")
+export let pokeNumber2 = document.getElementById("number2")
+export let pokeName2 = document.getElementById("name2")
+export let pokeType2 = document.getElementById("type2")
